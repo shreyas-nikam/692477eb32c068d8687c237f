21fc1ba0820f474bc73be8ca8c4bd92b976231d2
Shreyas Nikam <158344330+shreyas-nikam@users.noreply.github.com>
2025-11-25 13:03:51 -0500

    Add app.py

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
-# Starter Python file
+
+import streamlit as st
+st.set_page_config(page_title="GemmaVision-QuantAdvisor", layout="wide")
+st.sidebar.image("https://www.quantuniversity.com/assets/img/logo5.jpg")
+st.sidebar.divider()
+st.title("GemmaVision-QuantAdvisor")
+st.divider()
+
+st.markdown("""
+In this lab, we will explore the GemmaVision-QuantAdvisor application, designed for Financial Data Engineers to evaluate and compare Gemma 3 models. This platform offers insights into multimodal capabilities, quantization strategies, and performance benchmarks, facilitating informed deployment decisions for financial document understanding tasks.
+""")
+
+page = st.sidebar.selectbox(label="Navigation", options=["Home", "Model Overview", "Quantization Strategies"])
+
+if page == "Home":
+    from application_pages.home_page import main
+    main()
+elif page == "Model Overview":
+    from application_pages.model_overview import main
+    main()
+elif page == "Quantization Strategies":
+    from application_pages.quantization_strategies import main
+    main()
